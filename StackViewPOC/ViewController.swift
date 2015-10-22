e46e71aa8405139460aa83f2d412df554c3b854f
Javier <javier.loucim@gmail.com>
2015-10-22 08:45:57 -0300

    old school method

--- StackViewPOC/ViewController.swift
+++ StackViewPOC/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var stackView: UIStackView!
 
+    @IBOutlet weak var manualStackView: UIView!
 
     override func viewDidLoad() {
         
@@ -28,13 +29,42 @@ class ViewController: UIViewController {
         stackView.addArrangedSubview(view1)
         stackView.addArrangedSubview(view2)
         stackView.addArrangedSubview(view3)
-        
+
+
+        addViews()
+
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
         
-//        stackView.distribution = UIStackViewDistribution.EqualCentering
     }
 
+    override func viewDidAppear(animated: Bool) {
+    }
+
+    func addViews() {
+        let view1 = UIView(frame: CGRectMake(0, 0, 50, 30))
+        view1.backgroundColor = UIColor.redColor()
+        view1.translatesAutoresizingMaskIntoConstraints = false
+        
+        let view2 = UIView(frame: CGRectMake(0, 0, 50, 30))
+        view2.backgroundColor = UIColor.greenColor()
+        view2.translatesAutoresizingMaskIntoConstraints = false
+        
+        let view3 = UIView(frame: CGRectMake(0, 0, 50, 30))
+        view3.backgroundColor = UIColor.blueColor()
+        view3.translatesAutoresizingMaskIntoConstraints = false
+        
+        manualStackView.addSubview(view1)
+        manualStackView.addSubview(view2)
+        manualStackView.addSubview(view3)
+        
+        let constraints = NSLayoutConstraint.constraintsForEvenDistributionOfItems([view1,view2,view3], relativeToCenterOfItem: manualStackView, vertically: false) as! Array<NSLayoutConstraint>
+        
+        manualStackView.addConstraints(constraints)
+        
+        manualStackView.layoutIfNeeded()
+    }
     
     @IBAction func addViewToStack(sender: AnyObject) {
         
